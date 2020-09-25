8dfadcd5a7c4a6072ac73eb0fb68dd385ec3128c
Coder_WangChneyang <1351093262@qq.com>
2020-09-25 19:03:19 +0800

    传智健康

--- health_parent/health_common/src/main/java/com/itheima/utils/SMSUtils.java
+++ health_parent/health_common/src/main/java/com/itheima/utils/SMSUtils.java
@@ -13,8 +13,8 @@ import com.aliyuncs.profile.IClientProfile;
  * 短信发送工具类
  */
 public class SMSUtils {
-	public static final String VALIDATE_CODE = "SMS_159620392";//发送短信验证码
-	public static final String ORDER_NOTICE = "SMS_159771588";//体检预约成功通知
+	public static final String VALIDATE_CODE = "SMS_203670009";//发送短信验证码
+	public static final String ORDER_NOTICE = "SMS_203660028";//体检预约成功通知
 
 	/**
 	 * 发送短信
@@ -30,8 +30,8 @@ public class SMSUtils {
 		final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
 		final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
 		// 替换成你的AK
-		final String accessKeyId = "LTAIak3CfAehK7cE";// 你的accessKeyId,参考本文档步骤2
-		final String accessKeySecret = "zsykwhTIFa48f8fFdU06GOKjHWHel4";// 你的accessKeySecret，参考本文档步骤2
+		final String accessKeyId = "LTAI4GBH6nJ5wGqHgaAhiyZp";// 你的accessKeyId,参考本文档步骤2
+		final String accessKeySecret = "ZMGNnTg9kSZgW0wuL5fCdAF7rCCecd";// 你的accessKeySecret，参考本文档步骤2
 		// 初始化ascClient,暂时不支持多region（请勿修改）
 		IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
 		DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
@@ -43,7 +43,7 @@ public class SMSUtils {
 		// 必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
 		request.setPhoneNumbers(phoneNumbers);
 		// 必填:短信签名-可在短信控制台中找到
-		request.setSignName("传智健康");
+		request.setSignName("项目一传智健康");
 		// 必填:短信模板-可在短信控制台中找到
 		request.setTemplateCode(templateCode);
 		// 可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
