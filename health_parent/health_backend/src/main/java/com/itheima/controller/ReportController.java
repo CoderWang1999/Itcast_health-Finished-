018e9173070d1a5138760e6c6a99abdc09855ba1
Coder_WangChneyang <1351093262@qq.com>
2020-10-05 20:27:16 +0800

    传智健康

--- health_parent/health_backend/src/main/java/com/itheima/controller/ReportController.java
+++ health_parent/health_backend/src/main/java/com/itheima/controller/ReportController.java
@@ -6,12 +6,18 @@ import com.itheima.entity.Result;
 import com.itheima.service.BusinessReportService;
 import com.itheima.service.MemberService;
 import com.itheima.service.SetmealService;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
@@ -30,6 +36,7 @@ public class ReportController {
     private SetmealService setmealService;
     @Reference
     private BusinessReportService businessReportService;
+
     //会员数量折线图
     @RequestMapping("/getMemberReport")
     public Result getMemberReport() {
@@ -64,16 +71,16 @@ public class ReportController {
         try {
             //使用模拟数据测试使用什么样的java对象转为饼形图所需的json数据格式
             Map<String, Object> data = new HashMap<>();
-            List<Map<String,Object>> setmealCount=setmealService.findSetmealCount();
-            data.put("setmealCount",setmealCount);
-            List<String> setmealNames=new ArrayList<>();
+            List<Map<String, Object>> setmealCount = setmealService.findSetmealCount();
+            data.put("setmealCount", setmealCount);
+            List<String> setmealNames = new ArrayList<>();
             for (Map<String, Object> map : setmealCount) {
                 //获取套餐名称
-                String name= (String) map.get("name");
+                String name = (String) map.get("name");
                 setmealNames.add(name);
             }
-            data.put("setmealNames",setmealNames);
-            return new Result(true,MessageConstant.GET_SETMEAL_COUNT_REPORT_SUCCESS,data);
+            data.put("setmealNames", setmealNames);
+            return new Result(true, MessageConstant.GET_SETMEAL_COUNT_REPORT_SUCCESS, data);
         } catch (Exception e) {
             e.printStackTrace();
             return new Result(false, MessageConstant.GET_SETMEAL_COUNT_REPORT_FAIL);
@@ -82,18 +89,19 @@ public class ReportController {
 
     //运营数据统计
     @RequestMapping("/getBusinessReportData")
-    public Result getBusinessReportData(){
+    public Result getBusinessReportData() {
         try {
-            Map<String,Object> data=businessReportService.getBusinessReportData();
-            return new Result(true, MessageConstant.GET_BUSINESS_REPORT_SUCCESS,data);
-        }catch (Exception e){
+            Map<String, Object> data = businessReportService.getBusinessReportData();
+            return new Result(true, MessageConstant.GET_BUSINESS_REPORT_SUCCESS, data);
+        } catch (Exception e) {
             e.printStackTrace();
-            return new Result(false,MessageConstant.GET_BUSINESS_REPORT_FAIL);
+            return new Result(false, MessageConstant.GET_BUSINESS_REPORT_FAIL);
         }
     }
+
     //导出运营数据
     @RequestMapping("/exportBusinessReport")
-    public Result exportBusinessReport(HttpServletRequest request, HttpServletResponse response){
+    public Result exportBusinessReport(HttpServletRequest request, HttpServletResponse response) {
         try {
             Map<String, Object> result = businessReportService.getBusinessReportData();
             //取出返回结果数据，准备将报表数据写入到Excel文件中
@@ -110,7 +118,7 @@ public class ReportController {
             Integer thisMonthVisitsNumber = (Integer) result.get("thisMonthVisitsNumber");
             List<Map> hotSetmeal = (List<Map>) result.get("hotSetmeal");
             //设置文件路径
-            String filePath = request.getSession().getServletContext().getRealPath("template")+ File.separator+"report_template.xlsx";
+            String filePath = request.getSession().getServletContext().getRealPath("template") + File.separator + "report_template.xlsx";
             //基于提供的Excel模板文件在内存中创建一个Excel表格对象
             XSSFWorkbook excel = new XSSFWorkbook(new FileInputStream(new File(filePath)));
             //读取第一个工作表
@@ -139,11 +147,11 @@ public class ReportController {
             row.getCell(5).setCellValue(thisMonthOrderNumber);//本月预约数
             row.getCell(7).setCellValue(thisMonthVisitsNumber);//本月到诊数
             int rowNum = 12;
-            for(Map map : hotSetmeal){//热门套餐
+            for (Map map : hotSetmeal) {//热门套餐
                 String name = (String) map.get("name");
                 Long setmeal_count = (Long) map.get("setmeal_count");
                 BigDecimal proportion = (BigDecimal) map.get("proportion");
-                row = sheet.getRow(rowNum ++);
+                row = sheet.getRow(rowNum++);
                 row.getCell(4).setCellValue(name);//套餐名称
                 row.getCell(5).setCellValue(setmeal_count);//预约数量
                 row.getCell(6).setCellValue(proportion.doubleValue());//占比
@@ -156,10 +164,38 @@ public class ReportController {
             out.flush();
             out.close();
             excel.close();
-            return new Result(true,MessageConstant.GET_BUSINESS_REPORT_SUCCESS);
-        }catch (Exception e){
+            return new Result(true, MessageConstant.GET_BUSINESS_REPORT_SUCCESS);
+        } catch (Exception e) {
             e.printStackTrace();
-            return new Result(false,MessageConstant.GET_BUSINESS_REPORT_FAIL);
+            return new Result(false, MessageConstant.GET_BUSINESS_REPORT_FAIL);
+        }
+    }
+
+    //导出PDF
+    @RequestMapping("/exportBusinessReport4PDF")
+    public Result exportBusinessReport4PDF(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Map<String, Object> result = businessReportService.getBusinessReportData();
+            List<Map> hotSetmeal = (List<Map>) result.get("hotSetmeal");
+            //动态获取pdf模板文件绝对磁盘路径
+            String jrxmlPath = request.getSession().getServletContext().getRealPath("template") + File.separator + "health_business3.jrxml";
+            String jasperPath = request.getSession().getServletContext().getRealPath("template") + File.separator + "health_business3.jasper";
+            //编译模板
+            JasperCompileManager.compileReportToFile(jrxmlPath, jasperPath);
+            //填充数据---使用JavaBean数据源方式填充
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperPath, result, new JRBeanCollectionDataSource(hotSetmeal));
+            //创建输出流，用于从服务器写数据到客户端浏览器
+            ServletOutputStream out = response.getOutputStream();
+            response.setContentType("application/pdf");
+            response.setHeader("content-Disposition", "attachment;filename=report.pdf");
+            //输出文件
+            JasperExportManager.exportReportToPdfStream(jasperPrint,out);
+            out.flush();
+            out.close();
+            return new Result(true, MessageConstant.GET_BUSINESS_REPORT_SUCCESS);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, MessageConstant.GET_BUSINESS_REPORT_FAIL);
         }
     }
 }
